e833f24b600b4d2f7ea4953fb290140d543ebbc8
hitepa <hitesh.engineer85@gmail.com>
2014-06-18 18:15:10 +0530

    Added Properties file

--- src/com/impetus/eej2/cache/dao/EIECacheDaoImpl.java
+++ src/com/impetus/eej2/cache/dao/EIECacheDaoImpl.java
@@ -1,5 +1,16 @@
 package com.impetus.eej2.cache.dao;
 
+import com.impetus.eej2.cache.properties.EIECacheProperties;
+
 public class EIECacheDaoImpl {
 
+	private EIECacheProperties cacheProp;
+
+	public EIECacheProperties getCacheProp() {
+		return cacheProp;
+	}
+
+	public void setCacheProp(EIECacheProperties cacheProp) {
+		this.cacheProp = cacheProp;
+	}
 }
